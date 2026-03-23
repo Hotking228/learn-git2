ceae6b4ee537403f4d6ff0382716a7a2d221cd1a
Hotking228 <kraulov@bk.ru>
2026-03-23 12:12:23 +0300

    Initial commit

---
+++ src/main/java/com/hotking/learngit2/LearnGit2Application.java
@@ -0,0 +1,13 @@
+package com.hotking.learngit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGit2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnGit2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hotking/learngit2/LearnGit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotking.learngit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGit2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
